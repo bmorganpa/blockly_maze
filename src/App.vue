da0d439f805787b5fba229ac30bdb86aaed85229
Brad Morgan <bmorgan1@urbn.com>
2021-12-09 21:54:37 -0500

    Added custom moveForward block

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,28 @@
 
 <script>
 import BlocklyComponent from './components/BlocklyComponent.vue'
+import Blockly from 'blockly';
 import BlocklyJS from 'blockly/javascript';
+
+Blockly.Blocks['maze_moveForward'] = {
+  /**
+   * Block for moving forward.
+   * @this {Blockly.Block}
+   */
+  init: function() {
+    this.jsonInit({
+      "message0": "Move forward",
+      "previousStatement": null,
+      "nextStatement": null,
+      "colour": 230,
+      "tooltip": "This moves the character forward in the direction the are currently facing"
+    });
+  }
+};
+
+Blockly.JavaScript['maze_moveForward'] = function(block) {
+  return 'moveForward(\'block_id_' + block.id + '\');\n';
+};
 
 export default {
   name: 'app',
@@ -31,42 +52,7 @@ export default {
           },
         toolbox:
         `<xml>
-          <category name="Logic" colour="%{BKY_LOGIC_HUE}">
-            <block type="controls_if"></block>
-            <block type="logic_compare"></block>
-            <block type="logic_operation"></block>
-            <block type="logic_negate"></block>
-            <block type="logic_boolean"></block>
-          </category>
-          <category name="Loops" colour="%{BKY_LOOPS_HUE}">
-            <block type="controls_repeat_ext">
-              <value name="TIMES">
-                <block type="math_number">
-                  <field name="NUM">10</field>
-                </block>
-              </value>
-            </block>
-            <block type="controls_whileUntil"></block>
-          </category>
-          <category name="Math" colour="%{BKY_MATH_HUE}">
-            <block type="math_number">
-              <field name="NUM">123</field>
-            </block>
-            <block type="math_arithmetic"></block>
-            <block type="math_single"></block>
-          </category>
-          <category name="Text" colour="%{BKY_TEXTS_HUE}">
-            <block type="text"></block>
-            <block type="text_length"></block>
-            <block type="text_print"></block>
-          </category>
-          <category name="Variables" custom="VARIABLE" colour="%{BKY_VARIABLES_HUE}">
-            </category>
-          <category name="Stocks" colour="%{BKY_LOOPS_HUE}">
-            <block type="stock_buy_simple"></block>
-            <block type="stock_buy_prog"></block>
-            <block type="stock_fetch_price"></block>
-          </category>
+          <block type="maze_moveForward"></block>
         </xml>`
       }
     }
